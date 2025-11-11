7f5dd61c95afb1d21815c431f588f1c78ec9582d
SubhankarDas2004 <162700367+SubhankarDas2004@users.noreply.github.com>
2025-11-11 21:04:28 +0530

    Add files via upload

---
+++ Python/temperature.py
@@ -0,0 +1,6 @@
+#taking temperature in fahrenheit
+f=float(input("enter temperature in fahrenheit:"))
+#calculating by implementing formula
+c=(f-32)*(5/9)
+#printing
+print("temperature in centigrade is:",c)
