a44680f8af1bf8ee88ba1a633c914e74857cc94d
SubhankarDas2004 <162700367+SubhankarDas2004@users.noreply.github.com>
2025-11-11 20:57:13 +0530

    Add files via upload

---
+++ Python/aggregate_&_percentage.py
@@ -0,0 +1,10 @@
+print("Enter marks")
+a=float(input("enter marks for subject 1:"))
+b=float(input("enter marks for subject 2:"))
+c=float(input("enter marks for subject 3:"))
+d=float(input("enter marks for subject 4:"))
+e=float(input("enter marks for subject 5:"))
+aggre=a+b+c+d+e
+percent=(aggre/500)*100
+print("aggregate=",aggre)
+print("percent=",percent)
